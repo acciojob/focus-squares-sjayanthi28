9105c1789e27cc319e5a5b2c14f609090d54c06a
sjayanthi28 <sjayanthisjayanthi88@gmail.com>
2026-05-22 16:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,33 @@
-let square1 = document.getElementById("square1")
-let square2 = document.getElementById("square2")
-let square3 = document.getElementById("square3")
+let square1 = document.getElementById("square1");
+let square2 = document.getElementById("square2");
+let square3 = document.getElementById("square3");
 
-square1.addEventListener("mouseover",()=>{
+square1.addEventListener("mouseover", () => {
+    square2.classList.add("coffee");
+    square3.classList.add("coffee");
+});
 
-    square2.classList.add("coffee")
-    square3.classList.add("coffee")
+square1.addEventListener("mouseout", () => {
+    square2.classList.remove("coffee");
+    square3.classList.remove("coffee");
+});
 
-})
+square2.addEventListener("mouseover", () => {
+    square1.classList.add("coffee");
+    square3.classList.add("coffee");
+});
 
-square1.addEventListener("mouseout",()=>{
+square2.addEventListener("mouseout", () => {
+    square1.classList.remove("coffee");
+    square3.classList.remove("coffee");
+});
 
-    square2.classList.remove("coffee")
-    square3.classList.remove("coffee")
+square3.addEventListener("mouseover", () => {
+    square1.classList.add("coffee");
+    square2.classList.add("coffee");
+});
 
-})
-
-square2.addEventListener("mouseover",()=>{
-
-    square1.classList.add("coffee")
-    square3.classList.add("coffee")
-
-})
-
-square2.addEventListener("mouseout",()=>{
-
-    square1.classList.remove("coffee")
-    square3.classList.remove("coffee")
-
-})
-
-square3.addEventListener("mouseover",()=>{
-
-    square1.classList.add("coffee")
-    square2.classList.add("coffee")
-
-})
-
-square3.addEventListener("mouseout",()=>{
-
-    square1.classList.remove("coffee")
-    square2.classList.remove("coffee")
-
-})
+square3.addEventListener("mouseout", () => {
+    square1.classList.remove("coffee");
+    square2.classList.remove("coffee");
+});
